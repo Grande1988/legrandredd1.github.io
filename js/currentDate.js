93d73955239bd70a36066942a77fba3d41282a4f
legrandredd <legrandredd@byui.edu>
2021-01-04 16:59:48 -0700

    currentDate.js

---
+++ js/currentDate.js
@@ -0,0 +1,4 @@
+now= new Date();
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+date= now.getDate() + ", "+ now.getMonth() +" "+ now.getFullYear();
+document.getElementById("current-date").innerText= now.toLocaleDateString("en-GB", options );
