1e0a68768104e4736deea267d88d1bf31a3fb325
legrandredd <legrandredd@byui.edu>
2021-02-11 20:18:42 -0700

    windchill.js

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,11 +1,27 @@
-const temperature = document.getElementById('temperature').innerHTML;
-const windspeed = document.getElementById('windspeed').innerHTML;
+function isWindChill(){
+   let temperature = parseInt(document.querySelector("#temperature").textContent);
+   let windSpeed = parseInt(document.querySelector("#windSpeed").textContent);
+
+   if(temperature <= 50 && windSpeed >= 3.0){
+       windChill()
+   }
+   else{
+       document.querySelector("#windChill").innerHTML = "N/A"
+   }
+
+}
+
+function windChill(){
+// gets the content inside of the spans for temperature and wind speed
+let temperature = document.querySelector("#temperature").textContent;
+let windSpeed = document.querySelector("#windSpeed").textContent;
+
+wind_chill = 35.74 + (0.6215 * temperature) - (35.75 * (Math.pow(windSpeed, 0.16))) + (0.4275 *(temperature * (Math.pow(windSpeed, 0.16))));
+wind_chill = Math.round(wind_chill) + "&#176;F";
 
 
-let windchill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windspeed, .16)) + (0.4275 * temperature * Math.pow(windspeed, .16));
-  if (temperature <= 50 && windspeed > 3) {
-     windchill = Math.round(windchill);
-  } else {
-     windchill = "N/A";
-  }
-document.getElementById('windchill').innerHTML = windchill;
+return document.querySelector("#windChill").innerHTML = wind_chill;
+
+}
+
+isWindChill();
