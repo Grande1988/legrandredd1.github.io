b6b6278390c13cce3a29c500d8233e3cc9c62ca4
legrandredd <legrandredd@byui.edu>
2021-02-15 22:24:44 -0700

    hopefully added correct windChill.js

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,8 +1,14 @@
-  // Windchill calculation and display:
-  if (tempF <= 50 && speed > 3.0){
-    let f = 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 *tempF * Math.pow(speed, 0.16);
-    let feeling= Math.round(f * 100) / 100;
-    document.getElementById("windChill").innerText = feeling;
-    }else{
-    document.getElementById("windChill").innerText = "N/A";
-    }  
+let speed = parseFloat(document.querySelector("#speed").textContent);
+let temp = parseFloat(document.querySelector("#temp").textContent);
+document.getElementById("windchill").innerHTML = windChill(temp, speed);
+
+function windChill(temp, speed) {
+    if (temp <= 50 && speed > 3){
+    let end = Math.round(35.74 + .6215*temp - 35.75*speed**.16 + .4275*temp*speed**.16) + "&#8457;";
+    return end;
+    }
+    else
+    {
+        return "N/A";
+    }
+}
