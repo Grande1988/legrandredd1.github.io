3af547f5d1b8b636f77af87cd46775850a6251b8
legrandredd <legrandredd@byui.edu>
2021-02-15 18:29:23 -0700

    windchill.js

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,28 +1,8 @@
-/* Input: get a temperature from the first text field
-            get a wind speed from the second text field
- * Processing: call the second function (windChill)
-            store the value returned by windChill in a variable
- * Output: output the value returned by windChill to the div for the user to see
- */
-
-    
- 
-function doInputOutput() {
-    let tempF = parseFloat(document.getElementById('highnmb').innerHTML); 
-    let speed = parseFloat(document.getElementById('wndmph').innerHTML); 
-   let wCOutput = windChill(tempF, speed);
-   document.getElementById("outputDiv").innerHTML= wCOutput+"F"+"&deg;";
-}
-
-
-// call the second function (windChill)
-// store the value returned by windChill in a variable
-// output the value returned by windChill to the div for the user to see
-function windChill(T, S) {
- let wC  =  35.74 + 0.6215*T-35.75*(Math.pow(S, 0.16)) + 0.4275*T*(Math.pow(S, 0.16));
- let rwC = Math.round(wC);
-
- return rwC;
-}
-
-
+  // Windchill calculation and display:
+  if (tempF <= 50 && speed > 3.0){
+    let f = 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 *tempF * Math.pow(speed, 0.16);
+    let feeling= Math.round(f * 100) / 100;
+    document.getElementById("windChill").innerText = feeling;
+    }else{
+    document.getElementById("windChill").innerText = "N/A";
+    }  
