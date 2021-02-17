2d1a2cfcb24330ab6202e55f8b62ee29dca23407
legrandredd <legrandredd@byui.edu>
2021-02-17 15:35:30 -0700

    windchill

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,14 +1,11 @@
-let speed = parseFloat(document.querySelector("#speed").textContent);
-let temp = parseFloat(document.querySelector("#temp").textContent);
-document.getElementById("windchill").innerHTML = windChill(temp, speed);
+const temp = document.getElementById('temp').innerHTML;
+const windspeed = document.getElementById('windspeed').innerHTML;
 
-function windChill(temp, speed) {
-    if (temp <= 50 && speed > 3){
-    let end = Math.round(35.74 + .6215*temp - 35.75*speed**.16 + .4275*temp*speed**.16) + "&#8457;";
-    return end;
-    }
-    else
-    {
-        return "N/A";
-    }
-}
+
+let windchill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * temp * Math.pow(windspeed, 0.16));
+  if (temp <= 50 && windspeed > 3) {
+     windchill = Math.round(windchill);
+  } else {
+     windchill = "N/A";
+  }
+document.getElementById('windchill').innerHTML = windchill;
